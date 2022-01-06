940c32f9378cee7190ecc8c0b270aa4d44ae43e9
Jitesh Shaw <jiteshshaw93@gmail.com>
2022-01-06 16:31:19 +0530

    Optional basics

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,28 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Optional<String> empty = Optional.of("Hello");
-        System.out.println(empty.isPresent());
+        Optional<String> hello = Optional.of("Hello");
+        Optional<String> empty = Optional.empty();
+
+        System.out.println(hello.isPresent());
         System.out.println(empty.isEmpty());
+
+        System.out.println(hello.orElse("world"));
+        System.out.println(empty.orElse("there"));
+
+        System.out.println(hello.map(String::toUpperCase)
+                .orElseGet(() -> {
+                    // ... extra computation to retrieve the value
+                    return "world";
+                }));
+
+        try {
+            System.out.println(empty.map(String::toLowerCase)
+                    .orElseThrow(IllegalStateException::new));
+        } finally {
+            empty.ifPresent(System.out::println);
+
+            empty.ifPresentOrElse(System.out::println, () -> System.out.println("empty"));
+        }
     }
 }
